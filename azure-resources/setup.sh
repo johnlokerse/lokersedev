d5a719d7113b5792d2b2fec68722ee024fef9069
John <J.G.Lokerse@gmail.com>
2021-01-30 17:58:34 +0100

    Removed multi-line

--- azure-resources/setup.sh
+++ azure-resources/setup.sh
@@ -4,11 +4,4 @@ rg="rg-lokersedev"
 webAppName="lokerse-dev-staticapp"
 
 az group create --name $rg --location "westeurope"
-
-az staticwebapp create \ 
---resource-group $rg \
---location "westeurope" \
---name $webAppName \
---source "https://github.com/johnlokerse/lokersedev" \
---branch "master" \
---token __token__ 
+az staticwebapp create --resource-group $rg --location "westeurope" --name $webAppName --source "https://github.com/johnlokerse/lokersedev" --branch "master" --token __token__
