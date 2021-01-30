1671d4d675fe29f8a400d7adc888cbcc4ef99b3a
John <J.G.Lokerse@gmail.com>
2021-01-30 18:00:40 +0100

    YAML debugging

--- azure-resources/setup.sh
+++ azure-resources/setup.sh
@@ -2,6 +2,5 @@
 
 rg="rg-lokersedev"
 webAppName="lokerse-dev-staticapp"
-
 az group create --name $rg --location "westeurope"
 az staticwebapp create --resource-group $rg --location "westeurope" --name $webAppName --source "https://github.com/johnlokerse/lokersedev" --branch "master" --token __token__
